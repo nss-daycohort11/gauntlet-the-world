2737622e299fffb6d1cb75ba1eac1a9d9e64bf3c
MAOstrander <mathew.ostrander@gmail.com>
2015-10-24 23:29:00 -0500

    added the stealth class

--- javascripts/classes.js
+++ javascripts/classes.js
@@ -2,10 +2,11 @@
   Base function for a player, or enemy, class (profession)
  */
 var PlayerClass = function() {
-  this.name = "Beggar";
+  this.name = "Loser";
   this.healthBonus = 0;
   this.strengthBonus = 0;
   this.intelligenceBonus = 0;
+  this.evadeBonus = 0;
   this.magical = false;
 
   this.toString = function() {
@@ -28,7 +29,7 @@ Fighter.prototype = new PlayerClass();
 
 
 var Warrior = function() {
-  this.name = "Warrior";
+  this.name = "Scott Pilgrim"; //Warrior
   this.healthBonus = this.healthBonus + 25;
   this.strengthBonus = this.strengthBonus + 30;
 };
@@ -36,7 +37,7 @@ Warrior.prototype = new Fighter();
 
 
 var Valkyrie = function() {
-  this.name = "Valkyrie";
+  this.name = "Ramona Flowers"; //Valkyrie
   this.healthBonus = this.healthBonus + 20;
   this.strengthBonus = this.strengthBonus + 10;
 };
@@ -44,9 +45,9 @@ Valkyrie.prototype = new Fighter();
 
 
 var Berserker = function() {
-  this.name = "Berserker";
+  this.name = "NegaScott"; //Berserker
   this.healthBonus = this.healthBonus + 35;
-  this.strengthBonus = this.strengthBonus + 20;
+  this.strengthBonus = this.strengthBonus + 30;
 };
 Berserker.prototype = new Fighter();
 
@@ -66,40 +67,40 @@ Monk.prototype = new Fighter();
       - Conujurer
       - Sorcerer
  */
-var Mage = function() {
-  this.name = "Mage";
+var Musician = function() {
+  this.name = "Musician"; //Mage
   this.magical = true;
   this.healthBonus = this.healthBonus - 10;
   this.strengthBonus = this.strengthBonus - 20;
   this.intelligenceBonus = this.intelligenceBonus + 20;
 };
-Mage.prototype = new PlayerClass();
+Musician.prototype = new PlayerClass();
 
 
 var Shaman = function() {
-  this.name = "Shaman";
+  this.name = "Kim Pines"; //Shaman
   this.healthBonus = this.healthBonus + 5;
-  this.strengthBonus = this.strengthBonus - 10;
-  this.intelligenceBonus = this.intelligenceBonus + 20;
+  this.strengthBonus = this.strengthBonus - 5;
+  this.intelligenceBonus = this.intelligenceBonus + 25;
 };
-Shaman.prototype = new Mage();
+Shaman.prototype = new Musician();
 
 
 var Wizard = function() {
-  this.name = "Wizard";
+  this.name = "Steven Stills"; //Wizard
   this.healthBonus = this.healthBonus - 15;
   this.strengthBonus = this.strengthBonus - 25;
   this.intelligenceBonus = this.intelligenceBonus + 40;
 };
-Wizard.prototype = new Mage();
+Wizard.prototype = new Musician();
 
 
 var Conjurer = function() {
-  this.name = "Conjurer";
+  this.name = "Young Neil"; //Conjurer
   this.strengthBonus = this.strengthBonus - 10;
   this.intelligenceBonus = this.intelligenceBonus + 10;
 };
-Conjurer.prototype = new Mage();
+Conjurer.prototype = new Musician();
 
 
 var Sorcerer = function() {
@@ -108,13 +109,31 @@ var Sorcerer = function() {
   this.strengthBonus = this.strengthBonus - 20;
   this.intelligenceBonus = this.intelligenceBonus + 30;
 };
-Sorcerer.prototype = new Mage();
-
+Sorcerer.prototype = new Musician();
 
 /*
-    STEALTH CLASSES
+    Stealth CLASSES
       - Thief
       - Ninja
       - Assassin
  */
 
+var Stalker = function() {
+  this.strengthBonus = 10;
+  this.evadeBonus = 5;
+};
+Stalker.prototype = new PlayerClass();
+
+var Thief = function() {
+  this.name = "Wallace Wells"; //Thief
+  this.healthBonus = this.healthBonus + 5;
+  this.evadeBonus = this.evadeBonus + 15;
+};
+Thief.prototype = new Stalker();
+
+var Ninja = function() {
+  this.name = "Knives Chau"; //Ninja
+  this.strengthBonus = this.strengthBonus + 15;
+  this.evadeBonus = this.evadeBonus + 5;
+};
+Ninja.prototype = new Stalker();
